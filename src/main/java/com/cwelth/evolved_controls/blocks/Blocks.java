94930c0c1c84a19d7d197f67d925f100b9a3e68f
ZtH <junk@cwelth.com>
2015-10-25 21:13:10 +0300

    More blocks!

--- src/main/java/com/cwelth/evolved_controls/blocks/Blocks.java
+++ src/main/java/com/cwelth/evolved_controls/blocks/Blocks.java
@@ -2,8 +2,12 @@ package com.cwelth.evolved_controls.blocks;
 
 import com.cwelth.evolved_controls.blocks.renders.MRendererFancyButton;
 import com.cwelth.evolved_controls.blocks.renders.MRendererFancyHandle;
+import com.cwelth.evolved_controls.blocks.renders.MRendererKnifeSwitch;
+import com.cwelth.evolved_controls.blocks.renders.MRendererSwitchButton;
 import com.cwelth.evolved_controls.blocks.tileentities.TEFancyButton;
 import com.cwelth.evolved_controls.blocks.tileentities.TEFancyHandle;
+import com.cwelth.evolved_controls.blocks.tileentities.TEKnifeSwitch;
+import com.cwelth.evolved_controls.blocks.tileentities.TESwitchButton;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
@@ -25,6 +29,11 @@ public class Blocks {
         GameRegistry.registerBlock(new MBlockFancyHandle("fancy_handle", Material.rock), "fancy_handle");
         GameRegistry.registerTileEntity(TEFancyHandle.class, "fancy_handle");
 
+        GameRegistry.registerBlock(new MBlockKnifeSwitch("knife_switch", Material.rock), "knife_switch");
+        GameRegistry.registerTileEntity(TEKnifeSwitch.class, "knife_switch");
+
+        GameRegistry.registerBlock(new MBlockSwitchButton("switch_button", Material.rock), "switch_button");
+        GameRegistry.registerTileEntity(TESwitchButton.class, "switch_button");
 
         initClient();
     }
@@ -37,6 +46,12 @@ public class Blocks {
 
         MRendererFancyHandle rendererFancyHandle = new MRendererFancyHandle();
         rendererFancyHandle.registerFor(MBlockFancyHandle.class, TEFancyHandle.class);
+
+        MRendererKnifeSwitch rendererKnifeSwitch = new MRendererKnifeSwitch();
+        rendererKnifeSwitch.registerFor(MBlockKnifeSwitch.class, TEKnifeSwitch.class);
+
+        MRendererSwitchButton rendererSwitchButton = new MRendererSwitchButton();
+        rendererSwitchButton.registerFor(MRendererSwitchButton.class, TESwitchButton.class);
 
         System.out.println("initClient Called!");
     }
